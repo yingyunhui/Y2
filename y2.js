b89af5ea97bdca4a1765f59cf808d0c00bba6051
应云辉 <yingyunhui@hotmail.com>
2017-08-18 18:51:17 +0800

    update 17818

---
+++ y2.js
@@ -0,0 +1,34 @@
+/*
+Y2, a beautiful framework for canvas.
+code by yingyunhui, 2017.8.
+*/
+(function(window,undefined){
+    var $=function(e){
+        return new $.fn.init(e);
+    }
+    $.fn=$.prototype={
+        init:function(e){
+            ctx=document.getElementById(e).getContext("2d");
+        },
+        draw:function(p){
+            switch(p.name){
+                case "react":this.rect(p);break;
+                case "text":this.text(p);break;
+            }
+            return this;
+        },
+        rect:function(p){
+            var s=p.style;
+            ctx.fillStyle=s.color;
+            ctx.fillRect(s.left,s.top,s.width,s.height);
+        },
+        text:function(p){
+            var s=p.style;
+            ctx.font=s.font;
+            ctx.fillStyle=s.color;
+            ctx.fillText(p.data,s.left,s.top);
+        }
+    }
+    $.fn.init.prototype=$.fn;
+    window.Y2=window.y2=$;
+})(window)
